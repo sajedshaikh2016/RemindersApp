daca1951ec6083bd4fee34313a624e5ec23d3dd9
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2025-11-03 17:05:39 +0530

    Initial Commit

---
+++ RemindersApp/RemindersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersAppApp.swift
+//  RemindersApp
+//
+//  Created by Sajed Shaikh on 03/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RemindersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
